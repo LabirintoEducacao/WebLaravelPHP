70123eff62b902ffacd204915f98e49da6ad364a
gabriela <gftchian0609@gmail.com>
2019-08-06 10:44:59 -0300

    #1 sala publica

--- resources/views/virtual.blade.php
+++ resources/views/virtual.blade.php
@@ -42,6 +42,23 @@
     @endif
 
     @endforeach
+    @if($item->public==1)
+    <div class="col-md-3" style="padding-top:20px;">
+        <div class="card ">
+
+            <p class="card-title"> {{$item->name}} </p>
+
+            <img src=" {{ asset('img/1.jpg')}} " style="width:200px; margin-bottom: 10px; " alt="imagen labirinto">
+
+            <a href="#" class="btn btn-sm btn-outline-info fa fa-gamepad ">&ensp;Jogar</a>
+            <!----------------------Botao do Modal-------------------------->
+            <button type="button" class="btn btn-outline-cyan btn-sm fa fa-qrcode" data-toggle="modal" data-target="#salaModal" data-whatever="{{$item->id}}" data-whatevernome="{{$item->name}}"> Qr Code</button>
+
+
+        </div>
+    </div>
+
+    @endif
     @endforeach
 
 
