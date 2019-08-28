9507777484e8efb24f1f0fba924b7a5bc91035c3
Rafael Paiva <rafael.paiva.barbosa@gmail.com>
2019-08-28 14:48:28 -0300

    # Construindo a API de login, para que faça a autenticação com o jogo.

--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::get('/admin/editar-sala/{id}', 'PerguntaRespostaController@indexJson');
-Route::get('/admin/sala','SalaController@indexJson');
+Route::get('/sala','SalaController@indexJson');
+
+Route::Post('/login','SalaController@login');
+
+
 Route::get('/login', 'Auth\LoginController@teste');
 Route::Post('/virtual', 'SalaController@teste' );
+
+
+
 
 
 
