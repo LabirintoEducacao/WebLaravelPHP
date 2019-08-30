7dbeb29930024e21385cdcd860586bd0fd26f357
Rafael Paiva <rafael.paiva.barbosa@gmail.com>
2019-08-30 15:00:36 -0300

    # Arrumando QRCODE.

--- resources/views/virtual.blade.php
+++ resources/views/virtual.blade.php
@@ -8,7 +8,6 @@
             <h1>Jogos</h1>
         </div>
 
-
     </div>
 </div>
 <!------------------------ Espaço das Salas  --------------------------->
@@ -29,8 +28,6 @@
             <br><br>
 
             <a href="virtual/{{$item->id}}" class="btn btn-sm btn-outline-info fa fa-gamepad ">&ensp;Jogar</a>
-
-            
  
             <!----------------------Botao do Modal-------------------------->
             <button type="button" class="btn btn-outline-cyan btn-sm fa fa-qrcode" data-toggle="modal" data-target="#salaModal" data-whatever="{{$item->id}}" data-whatevernome="{{$item->name}}"> Qr Code</button>
@@ -42,12 +39,15 @@
     @foreach($sala_user as $sala)
     @if($item->id==$sala->sala_id)
     @if($user == $sala->user_id)
-    <?php $id = $item->id ?>
+    <?php $id=$item->id ?>
+
+
 
 <?php 
-
 $pasta = $_SERVER['DOCUMENT_ROOT'] . '/sala/'.$id; 
 if(!is_dir($pasta)) die("<h2>O caminho $pasta não existe</h2>");
+
+var_dump($pasta);
 
 
 $arquivos = glob("$pasta/{*.[pP][nN][gG]}", GLOB_BRACE);
@@ -55,57 +55,6 @@ $arquivos = glob("$pasta/{*.[pP][nN][gG]}", GLOB_BRACE);
 $i = 0;
 
 ?>
-</div>
-    <div class="col-md-3" style="padding-top:20px;">
-        <div class="card ">
-
-            <p class="card-title"> {{$item->name}} </p>
-
-            <img src=" {{ asset('img/1.jpg')}} " style="width:200px; margin-bottom: 10px; " alt="imagen labirinto">
-
-
-            <a href="virtual/{{$item->id}}" class="btn btn-sm btn-outline-info fa fa-gamepad ">&ensp;Jogar</a>
-
-            <!----------------------Botao do Modal-------------------------->
-            <button type="button" class="btn btn-outline-cyan btn-sm fa fa-qrcode" data-toggle="modal" data-target="#salaModal" data-whatever="{{$item->id}}" data-whatevernome="{{$item->name}}"> Qr Code</button>
-
-
-        </div>
-    </div>
-    @endif
-    @endif
-
-    @endforeach
-
-    @endif
-    @endforeach
-
-
-    <div class="modal fade " id="salaModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-scrollable"  role="document">
-            <div class="modal-content "  >
-                <div class="modal-header" style="background-color:#2F4F4F;">
-                    <h5 class="modal-title"></h5>
-                    <h5 style="  font-size: 20px;  margin-left:250px; color:#ffffff;
-        " id="exampleModalScrollableTitle"> </h5>
-
-                </div>
-                <div class="modal-body " >
-
-                    <h3 class="sala_name" align="center">Sala</h3> 
-
-<?php 
-$pasta = $_SERVER['DOCUMENT_ROOT'] . '/sala/'.$id; 
-if(!is_dir($pasta)) die("<h2>O caminho $pasta não existe</h2>");
-
-
-$arquivos = glob("$pasta/{*.[pP][nN][gG]}", GLOB_BRACE);
-
-$i = 0;
-
-?>
-
-
 
 <div id="carouselExampleControls"  class="carousel slide" data-ride="carousel">
   <div class="carousel-inner ">
@@ -141,11 +90,55 @@ $i = 0;
   </a>
 </div>
 
-</div>
-</div>
-</div>
-</div>
 
-@endsection
+    <div class="col-md-3" style="padding-top:20px;">
+        <div class="card ">
+
+            <p class="card-title"> {{$item->name}} </p>
+
+            <img src=" {{ asset('img/1.jpg')}} " style="width:200px; margin-bottom: 10px; " alt="imagen labirinto">
+
+
+            <a href="virtual/{{$item->id}}" class="btn btn-sm btn-outline-info fa fa-gamepad ">&ensp;Jogar</a>
+
+            <!----------------------Botao do Modal-------------------------->
+            <button type="button" class="btn btn-outline-cyan btn-sm fa fa-qrcode" data-toggle="modal" data-target="#salaModal" data-whatever="{{$item->id}}" data-whatevernome="{{$item->name}}"> Qr Code</button>
+
+
+        </div>
+    </div>
+    @endif
+    @endif
+
+    @endforeach
+
+    @endif
+    @endforeach
+
+
+    <div class="modal fade" id="salaModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable" role="document">
+            <div class="modal-content ">
+                <div class="modal-header" style="background-color:#2F4F4F;">
+                    <h5 class="modal-title"></h5>
+                    <h5 style="  font-size: 20px;  margin-left:250px;  color:#ffffff;
+        " id="exampleModalScrollableTitle">Qr Code </h5>
+
+                </div>
+                <div class="modal-body">
+
+                    <h3 class="sala_name" align="center">Sala</h3>
+                    <p style="padding-left: 170px;">{!! QrCode::size(250)->generate( 'http://127.0.0.1:8000/admin/virtual/1' ); !!}</p>
+                    <input type="hidden" name="sala_id" id="sala_id">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @endsection
 
 
