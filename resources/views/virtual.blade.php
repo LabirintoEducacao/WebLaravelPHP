ec3ce7e303fdb5480ab4a0863b87c8cbb483f317
gabriela <gftchian0609@gmail.com>
2019-09-04 16:21:58 -0300

    #core botoes carousel

--- resources/views/virtual.blade.php
+++ resources/views/virtual.blade.php
@@ -43,108 +43,61 @@
 
         
 <!-- Modal -------------------->
-
-
-    <div class="modal fade" id="md{{$item->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-scrollable" role="document">
-            <div class="modal-content ">
-                <div class="modal-header" style="background-color:#2F4F4F;">
-                    <h5 class="modal-title"></h5>
-
-                    <h5 style="  font-size: 20px;color:#ffffff;
-        " id="exampleModalScrollableTitle">Qr Code </h5>
-
+<div class="modal fade" id="md{{$item->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-content ">
+            <div class="modal-header" style="background-color:#2F4F4F;">
+                <h5 class="modal-title"></h5>
+                <h5 style="  font-size: 20px;color:#ffffff;" id="exampleModalScrollableTitle">Qr Code </h5>
+            </div>
+            <div class="modal-body">
+                <?php
+                $id = $item->id;
+                $pasta = $_SERVER['DOCUMENT_ROOT'] . '/sala/'.$id; 
+                if(!is_dir($pasta)) die("<h2>O caminho $pasta não existe</h2>");
+                $arquivos = glob("$pasta/{*.[pP][nN][gG]}", GLOB_BRACE);
+                $i = 0;
+                ?>
+                <div id="controlsmd{{$item->id}}" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner" style="margin-left:auto;margin-right:auto;display:block;">
+                        <h5> {{$item->name}} </h5>
+                        @foreach($arquivos as $img) 
+                            <?php 
+                                $b = explode('public/', $img,2);         
+                            ?>
+                            @if($i == 0)
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="{{ asset($b[1]) }}" alt="First slide" width="40%">
+                                <p> Qr Code: {{$i}} </p>
+                            </div>
+                            @endif
+                            @if($i > 0)
+                            <div class="carousel-item ">
+                                <img class="d-block w-100" src="{{ asset($b[1]) }}" alt="Second slide">
+                                <p> Qr Code: {{$i}} </p>
+                            </div>
+                            @endif   
+                            <?php 
+                            $i++; 
+                            ?>
+                        @endforeach
+                    </div>
+                    <a class="carousel-control-prev" href="#controlsmd{{$item->id}}" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Anterior</span>
+                    </a>
+                    <a class="carousel-control-next" href="#controlsmd{{$item->id}}" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"><i class="fas fa-arrow-circle-right"></i></span>
+                        <span class="sr-only">Próximo</span>
+                    </a>
                 </div>
-<div class="container row">
-
-    <div class="col-1"> 
-        <a class="carousel-control-prev" href="#controlsmd{{$item->id}}" role="button" data-slide="prev" style=" padding-left:30px; font-size: 20px;" >
-        <span class="fa fa-arrow-circle-left" style="color:#000000; " aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-        </a>
-    </div>
-
-
-
-  <div class="modal-body col-10">
-
-                 
-    <?php
-    $id = $item->id;
-
-    $pasta = $_SERVER['DOCUMENT_ROOT'] . '/sala/'.$id; 
-if(!is_dir($pasta)) die("<h2>O caminho $pasta não existe</h2>");
-
-$arquivos = glob("$pasta/{*.[pP][nN][gG]}", GLOB_BRACE);
-
-$i = 0;
-     
-?>
-
-
-<div id="controlsmd{{$item->id}}" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-
-<h5> {{$item->name}} </h5>
-
- @foreach($arquivos as $img) 
-
-<?php 
-          $b = explode('public/', $img,2);
-          
-?>
-
- @if($i == 0)
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="{{ asset($b[1]) }}" alt="First slide" width="40%">
-      <p> Qr Code: {{$i}} </p>
-    </div>
-   
-@endif
-   
-
-@if($i > 0)
-
-    <div class="carousel-item ">
-      <img class="d-block w-100" src="{{ asset($b[1]) }}" alt="Second slide">
-      <p> Qr Code: {{$i}} </p>
-    </div>
-
-
- @endif   
-
-
-
-<?php 
-$i++; 
-?>
-@endforeach
-
-
-
-</div>
-
-  
-</div>
-</div>
-
-<div class="col-1"> 
-    <a class="carousel-control-next" href="#controlsmd{{$item->id}}" role="button" data-slide="next" style=" padding-right:20px; font-size: 20px;">
-    <span class="fa fa-arrow-circle-right"  style="color:#000000;" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-
-</div>
-
+            </div>
             <div class="modal-footer">
-            <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
 </div>
-</div>
-</div>
-</div>
-
 <!--- Fim Modal --->
 
        
@@ -177,113 +130,61 @@ $i++;
 
 
  <!-- Modal -------------------->
-
-
-    <div class="modal fade" id="md{{$item->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-scrollable" role="document">
-            <div class="modal-content ">
-                <div class="modal-header" style="background-color:#2F4F4F;">
-                    <h5 class="modal-title"></h5>
-
-                    <h5 style="  font-size: 20px;color:#ffffff;
-        " id="exampleModalScrollableTitle">Qr Code </h5>
-
+<div class="modal fade" id="md{{$item->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-content ">
+            <div class="modal-header" style="background-color:#2F4F4F;">
+                <h5 class="modal-title"></h5>
+                <h5 style="  font-size: 20px;color:#ffffff;" id="exampleModalScrollableTitle">Qr Code </h5>
+            </div>
+            <div class="modal-body">
+                <?php
+                $id = $item->id;
+                $pasta = $_SERVER['DOCUMENT_ROOT'] . '/sala/'.$id; 
+                if(!is_dir($pasta)) die("<h2>O caminho $pasta não existe</h2>");
+                $arquivos = glob("$pasta/{*.[pP][nN][gG]}", GLOB_BRACE);
+                $i = 0;
+                ?>
+                <div id="controlsmd{{$item->id}}" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner" style="margin-left:auto;margin-right:auto;display:block;">
+                        <h5> {{$item->name}} </h5>
+                        @foreach($arquivos as $img) 
+                            <?php 
+                                $b = explode('public/', $img,2);         
+                            ?>
+                            @if($i == 0)
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="{{ asset($b[1]) }}" alt="First slide" width="40%">
+                                <p> Qr Code: {{$i}} </p>
+                            </div>
+                            @endif
+                            @if($i > 0)
+                            <div class="carousel-item ">
+                                <img class="d-block w-100" src="{{ asset($b[1]) }}" alt="Second slide">
+                                <p> Qr Code: {{$i}} </p>
+                            </div>
+                            @endif   
+                            <?php 
+                            $i++; 
+                            ?>
+                        @endforeach
+                    </div>
+                    <a class="carousel-control-prev" href="#controlsmd{{$item->id}}" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Anterior</span>
+                    </a>
+                    <a class="carousel-control-next" href="#controlsmd{{$item->id}}" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Próximo</span>
+                    </a>
                 </div>
-<div class="container row">
-
-    <div class="col-1"> 
-        <a class="carousel-control-prev" href="#controlsmd{{$item->id}}" role="button" data-slide="prev" style=" padding-left:30px; font-size: 20px;" >
-        <span class="fa fa-arrow-circle-left" style="color:#000000; " aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-        </a>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
     </div>
-
-
-
-  <div class="modal-body col-10">
-
-                 
-    <?php
-    $id = $item->id;
-
-    $pasta = $_SERVER['DOCUMENT_ROOT'] . '/sala/'.$id; 
-if(!is_dir($pasta)) die("<h2>O caminho $pasta não existe</h2>");
-
-$arquivos = glob("$pasta/{*.[pP][nN][gG]}", GLOB_BRACE);
-
-$i = 0;
-     
-?>
-
-
-<div id="controlsmd{{$item->id}}" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-
-<h5> {{$item->name}} </h5>
-
- @foreach($arquivos as $img) 
-
-<?php 
-          $b = explode('public/', $img,2);
-          
-?>
-
- @if($i == 0)
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="{{ asset($b[1]) }}" alt="First slide">
-      <p> Qr Code: {{$i}} </p>
-    </div>
-   
-@endif
-   
-
-@if($i > 0)
-
-    <div class="carousel-item ">
-      <img class="d-block w-100" src="{{ asset($b[1]) }}" alt="Second slide">
-      <p> Qr Code: {{$i}} </p>
-    </div>
-
-
- @endif   
-
-
-
-<?php 
-$i++; 
-?>
-@endforeach
-
-
-
 </div>
-
-  
-</div>
-</div>
-
-<div class="col-1"> 
-    <a class="carousel-control-next" href="#controlsmd{{$item->id}}" role="button" data-slide="next" >
-    <span class="fa fa-arrow-circle-right"  style="color:#000000;" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-
-</div>
-
-<div class="modal-footer">
- <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-
-</div>
-</div>
-</div>
-</div>
-
-
-
-
-
-
 
 <!--- Fim Modal --->
 
