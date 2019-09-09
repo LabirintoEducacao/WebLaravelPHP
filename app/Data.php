f568989975b80ed5b01effe1fdb5d83c0ea4413f
Rafael Paiva <rafael.paiva.barbosa@gmail.com>
2019-09-09 11:18:04 -0300

    # Controller Estatistica

---
+++ app/Data.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Data extends Model
+{
+    protected $fillable = [
+    'id', 'user_id', 'maze_id', 'question_id', 'answer_id','wrong_count','correct_count', 'correct', 
+    'elapsed_time', 'answers_read_count', 'async_timestamp'
+    ];
+}
---
+++ database/migrations/2019_09_06_175339_create_data_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        Schema::create('data', function (Blueprint $table) {
+          $table->bigIncrements('id');
+          $table->integer('user_id');
+          $table->integer('maze_id');
+          $table->integer('question_id');
+          $table->integer('answer_id')->nullable();
+          $table->integer('wrong_count')->nullable();
+          $table->integer('correct_count')->nullable();
+          $table->boolean('correct')->nullable();
+          $table->integer('elapsed_time');
+          $table->integer('answers_read_count')->nullable();
+          $table->time('async_timestamp');
+          $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data');
+    }
+}
