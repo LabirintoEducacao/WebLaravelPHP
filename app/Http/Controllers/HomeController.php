0752545cea650fafbf982807c3cfa1ed249fbf00
gabriela <gftchian0609@gmail.com>
2019-09-13 16:29:12 -0300

    #core erros e acertos por sala incompleto (home)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $salas = DB::table('salas')
+                ->where('prof_id','=',Auth::user()->id)
+                ->get();
+        return view('home')->with(['salas' => $salas]);
     }
 }
