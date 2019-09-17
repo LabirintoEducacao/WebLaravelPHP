bd601ebdcaa643cc7f6ae2281afa29bf55f6918d
gabriela <gftchian0609@gmail.com>
2019-09-17 12:31:07 -0300

    #core graficos dnv

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
       <div class="card-columns cols-2">
         <div class="card">
           <div class="card-header">
-            Line Chart
+            Acertos e erros por sala
             <div class="card-actions">
               <a href="http://www.chartjs.org">
                 <small class="text-muted">docs</small>
@@ -25,82 +25,7 @@
           </div>
           <div class="card-body">
             <div class="chart-wrapper">
-              <canvas id="canvas-1"></canvas>
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">
-            Bar Chart
-            <div class="card-actions">
-              <a href="http://www.chartjs.org">
-                <small class="text-muted">docs</small>
-              </a>
-            </div>
-          </div>
-          <div class="card-body">
-            <div class="chart-wrapper">
-              <canvas id="canvas-2"></canvas>
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">
-            Doughnut Chart
-            <div class="card-actions">
-              <a href="http://www.chartjs.org">
-                <small class="text-muted">docs</small>
-              </a>
-            </div>
-          </div>
-          <div class="card-body">
-            <div class="chart-wrapper">
-              <canvas id="canvas-3"></canvas>
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">
-            Radar Chart
-            <div class="card-actions">
-              <a href="http://www.chartjs.org">
-                <small class="text-muted">docs</small>
-              </a>
-            </div>
-          </div>
-          <div class="card-body">
-            <div class="chart-wrapper">
-              <canvas id="canvas-4"></canvas>
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">
-            Pie Chart
-            <div class="card-actions">
-              <a href="http://www.chartjs.org">
-                <small class="text-muted">docs</small>
-              </a>
-            </div>
-          </div>
-          <div class="card-body">
-            <div class="chart-wrapper">
-              <canvas id="canvas-5"></canvas>
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">
-            Polar Area Chart
-            <div class="card-actions">
-              <a href="http://www.chartjs.org">
-                <small class="text-muted">docs</small>
-              </a>
-            </div>
-          </div>
-          <div class="card-body">
-            <div class="chart-wrapper">
-              <canvas id="canvas-6"></canvas>
+              <canvas id="salas"></canvas>
             </div>
           </div>
         </div>
@@ -113,5 +38,63 @@
 @endsection
 
 @section('myscript')
-	<script src="{{ asset('admin/js/charts.js') }}"></script>
+<script>
+	var salas = <?php echo $salas; ?>;
+    var salasNome = [];
+    var salasId = [];
+
+    for (var i = 0; i < salas.length; i++) {
+        salasNome[i] = salas[i].pergunta;
+        salasId[i] = salas[i].id;
+        console.log(salasNome[i]);
+    }
+
+    function random() {
+        return Math.round(Math.random() * 100);
+    };
+
+
+    var ctxp = document.getElementById('salas').getContext('2d');
+    var barChart = new Chart(ctxp, {
+        type: 'horizontalBar',
+        data: {
+            labels: salasNome,
+            borderColor: 'rgba(255, 0, 0, 0.8)',
+            highlightFill: 'rgba(255, 0, 0, 0.75)',
+            highlightStroke: 'rgb(255, 0, 0)',
+            data: [random(), random()],
+        datasets: [{
+            label: 'Erros',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            borderColor: 'rgba(255, 0, 0, 0.8)',
+            highlightFill: 'rgba(255, 0, 0, 0.75)',
+            highlightStroke: 'rgb(255, 0, 0)',
+            data: [random(), random()]
+        }, {
+            label: 'Acertos',
+            backgroundColor: 'rgba(0, 255, 9, 0.5)',
+            borderColor: 'rgba(0, 255, 9, 0.8)',
+            highlightFill: 'rgba(0, 255, 9, 0.75)',
+            highlightStroke: 'rgba(0, 255, 9, 1)',
+            data: [random(), random()]
+        }]
+
+    },
+    options: {
+        tooltips: {
+            mode: 'index',
+            intersect: false
+        },
+        responsive: true,
+        scales: {
+            xAxes: [{
+                stacked: true,
+            }],
+            yAxes: [{
+                stacked: true
+            }]
+        }
+    }
+    });
+</script>
 @endsection
