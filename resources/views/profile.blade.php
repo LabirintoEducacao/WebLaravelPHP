90d8bd9b9cc48b43d5aa64135293247b04a19c93
Victor <vic.hugoalmeida@gmail.com>
2019-10-23 11:30:16 -0300

    #victor

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -37,10 +37,7 @@
 -->
 <!--                    </div>-->
                     <br>
-                           <div class="rox">
-                           <a class="btn btn-primary col-12" style="color:white"  data-toggle="modal" data-target="#alterSenha" onclick="document.getElementById('password_atual').removeAttribute('readonly')">ALTERAR SENHA</a>
-                               </div>
-                           <br>
+                           
 <!--
                         {{ Form::label('password', 'Senha:') }}
                         {{ Form::password('password',['class'=>'form-control', 'placeholder'=>'Senha','minlength'=>'8', 'required']) }}<br>
@@ -68,10 +65,70 @@
 
                  @endif
 
+
             </div>
+
+            <div class="rox">
+                           <a class="btn btn-primary col-12" style="color:white"  data-toggle="modal" data-target="#alterSenha" onclick="document.getElementById('password_atual').removeAttribute('readonly')">ALTERAR SENHA</a>
+                               </div>
+                           <br>
+
         </div>
     </div>
 
+ <form action="{{url('home')}}" method="POST" style="margin-left: 1%;margin-right:1%" autocomplete="false">
+                    <!--                        @method('POST')-->
+                    {{ csrf_field() }}
+
+                    <!--input type="hidden" name="_method" value="PATCH">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}"-->
+                                <div class="modal-body">
+                    <div class="form-group has-feedback {{ $errors->has('password') ? 'has-error' : '' }}">
+                        <input type="password" name="password_atual" id="password_atual" class="form-control" placeholder="Senha atual" autocomplete="false" readonly>
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                        @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                    <div class="form-group has-feedback {{ $errors->has('password') ? 'has-error' : '' }}">
+                        <input type="password" name="password" class="form-control" placeholder="Nova senha">
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                        @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                    <div class="form-group has-feedback {{ $errors->has('password_confirmation') ? 'has-error' : '' }}">
+                        <input type="password" name="password_confirmation" class="form-control" placeholder="Confirme sua nova senha">
+                        <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+                        @if ($errors->has('password_confirmation'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password_confirmation') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                                    </div>
+
+                    <!--
+                    {{ Form::label('password', 'Nova senha:') }}
+                    {{ Form::password('password',['class'=>'form-control', 'autofocus', 'placeholder'=>'Nova senha']) }}<br>
+                    {{ Form::label('password2', 'Confirmação de senha:') }}
+                    {{ Form::password('password2',['class'=>'form-control', 'placeholder'=>'Confirmação de senha']) }}<br>
+-->
+                    <!--
+                    {{ Form::label('password', 'Senha:') }}
+                    {{ Form::password('password',['class'=>'form-control', 'placeholder'=>'Senha','minlength'=>'8', 'required']) }}<br>
+-->             <div class="modal-footer">
+                    <div class="row align-self-center">
+                        
+                         <button type="button" class="btn btn-default col" data-dismiss="modal">Fechar</button>
+                        <button type="submit" class="btn btn-success col">ALTERAR SENHA</button>
+                    </div>
+                                 </div>
+                </form>
 </div>
 
 
@@ -88,7 +145,10 @@
                       <i class="material-icons">clear</i>
                     </button>
             </div>
-                            <form action="{{ url('home') }}" method="POST" style="margin-left: 1%;margin-right:1%" autocomplete="false">
+
+           
+
+                <form action="{{url('home')}}" method="POST" style="margin-left: 1%;margin-right:1%" autocomplete="false">
                     <!--                        @method('POST')-->
                     {{ csrf_field() }}
 
