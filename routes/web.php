8a25f58ba07b4c957ae52bdd1583d2fcc219a0c4
Victor <vic.hugoalmeida@gmail.com>
2019-11-08 11:51:32 -0300

    # victor

--- routes/web.php
+++ routes/web.php
@@ -199,6 +199,7 @@ Route::get('/newuser', function () {
 
 Route::get('/grupo/{id}','SalaController@showgrupos');
 Route::get('/addgrupo/{id}/{salaid}','SalaController@addgrupo');
+Route::get('/vinculogrupo/{id}','SalaController@vinculogrupo');
 
 
 
